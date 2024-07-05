e57d8b7c1bde463baa1db8bbff05609df57a19eb
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2024-07-06 00:02:28 +0200

    Update app.ts

--- JustDoIt/src/app.ts
+++ JustDoIt/src/app.ts
@@ -1,1 +1,12 @@
-console.log("Testing...");
+const toDoItemTemplate = document.getElementById("todo-item-template")! as HTMLTemplateElement;
+const toDoContainer = document.getElementById("todo-container")! as HTMLDivElement;
+const toDoModal = document.getElementById("modal")! as HTMLDivElement;
+
+(document.getElementById("add-btn")! as HTMLButtonElement).addEventListener("click", () => toDoModal.style.display = 'block');
+
+(document.getElementById("close-btn")! as HTMLButtonElement).addEventListener("click", () => toDoModal.style.display = 'none');
+
+window.addEventListener("click", (e: Event) => {
+    if (e.target == toDoModal) toDoModal.style.display = 'none';
+});
+
