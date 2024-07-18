d2b60e1e10095e5e4b464c66f14da9581d416754
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-07-18 19:53:18 +0200

    Update IStorage.ts

--- JustDoIt/src/interfaces/IStorage.ts
+++ JustDoIt/src/interfaces/IStorage.ts
@@ -1,4 +1,8 @@
 export default interface IStorage<T extends object> {
     Save(): void;
     Load(): T[];
-}
+    Clear(): void;
+    Add(obj: T): void;
+    Edit(obj: T): void;
+    Delete(id: number): void;
+};
