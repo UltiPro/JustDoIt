1e9cb826f2de982db41fa20b7504afab7e2dcd31
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-07-20 22:01:13 +0200

    Update app.ts

--- JustDoIt/src/app.ts
+++ JustDoIt/src/app.ts
@@ -1,15 +1,6 @@
+import { InitFormModal } from "./utils/FormModal";
 import ToDoList from "./ToDoList";
 
-const toDoItemTemplate = document.getElementById("todo-item-template")! as HTMLTemplateElement;
-const toDoContainer = document.getElementById("todo-container")! as HTMLDivElement;
-const toDoModal = document.getElementById("modal")! as HTMLDivElement;
+InitFormModal();
 
-(document.getElementById("add-btn")! as HTMLButtonElement).addEventListener("click", () => toDoModal.style.display = 'block');
-
-(document.getElementById("close-btn")! as HTMLButtonElement).addEventListener("click", () => toDoModal.style.display = 'none');
-
-window.addEventListener("click", (e: Event) => {
-    if (e.target == toDoModal) toDoModal.style.display = 'none';
-});
-
-const toDoList: ToDoList = ToDoList.Instance;
+ToDoList.Instance;
