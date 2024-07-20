83136ba9411a89c580a922426c6050af8427fe95
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-07-20 18:35:20 +0200

    Update IStorage.ts

--- JustDoIt/src/interfaces/IStorage.ts
+++ JustDoIt/src/interfaces/IStorage.ts
@@ -1,8 +1,10 @@
+import { ToDoType } from "../enums/ToDoType";
+
 export default interface IStorage<T extends object> {
     Save(): void;
     Load(): void;
     Clear(): void;
     Add(obj: T): void;
-    Edit(id: number, obj: T): void;
+    Edit(id: number, title?: string, description?: string, _toDoType?: ToDoType): void;
     Delete(id: number): void;
 };
