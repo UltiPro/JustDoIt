ba1b2a70ccc60cdbc8a45ec9df12f57bfd157762
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-07-21 18:58:56 +0200

    Update ToDoItem.ts

--- JustDoIt/src/models/ToDoItem.ts
+++ JustDoIt/src/models/ToDoItem.ts
@@ -1,12 +1,30 @@
-export default class ToDoItem {
+import BuildToDoItem from "../decorators/BuildToDoItem";
+import IIndexable from "../interfaces/IIndexable";
+import ToDoList from "../ToDoList";
+import { ToDoType } from "../enums/ToDoType";
+
+@BuildToDoItem()
+export default class ToDoItem implements IIndexable {
+    _id: number;
     private _title: string;
     private _description: string;
-    private _imgPath: string;
+    private _toDoType: ToDoType;
 
-    public constructor(title: string, description: string, imgPath: string){
-        this._title = title;
-        this._description = description;
-        this._imgPath = imgPath;
+    constructor(_title: string, _description: string, _toDoType: ToDoType);
+    constructor(_title: string, _description: string, _toDoType: ToDoType, _id: number);
+    constructor(_title: string, _description: string, _toDoType: ToDoType, _id?: number) {
+        this._id = _id ?? ToDoList.Instance.GenerateId();
+        this._title = _title;
+        this._description = _description;
+        this._toDoType = _toDoType;
+    }
+
+    public get Id(): number {
+        return this._id;
+    }
+
+    public set Id(_id: number) {
+        this._id = _id;
     }
 
     public get Title(): string {
@@ -14,7 +32,26 @@ export default class ToDoItem {
     }
 
     public set Title(_title: string) {
-        if(_title.length < 0)
+        if (_title.length < 3 || _title.length > 64)
             throw new Error("Title has to be minimum 3 characters long and maximum 64 characters long.");
+        this._title = _title;
+    }
+
+    public get Description(): string {
+        return this._description;
+    }
+
+    public set Description(_description: string) {
+        if (_description.length > 256)
+            throw new Error("Description has to be maximum 256 characters long.");
+        this._description = _description;
+    }
+
+    public get ToDoType(): ToDoType {
+        return this._toDoType;
+    }
+
+    public set ToDoType(_toDoType: ToDoType) {
+        this._toDoType = _toDoType;
     }
 }
