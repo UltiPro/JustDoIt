45f7970665f6294ec6939d8c7a2a4f229210a3fb
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-08-04 22:25:31 +0200

    Webpack production new config

--- JustDoIt/webpack.config.prod.js
+++ JustDoIt/webpack.config.prod.js
@@ -1,26 +1,50 @@
 const path = require("path");
-const CleanPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
     mode: 'production',
-    entry: "./src/app.ts",
+    entry: './src/app.ts',
     output: {
-        filename: "bundle.main.js",
+        filename: 'bundle.[fullhash].js',
         path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        minimizer: [
+            `...`,
+            new CssMinimizerPlugin()
+        ],
     },
     module: {
         rules: [
             {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: path.resolve(__dirname, 'img')
+                        },
+                    },
+                    "css-loader"
+                ],
+                exclude: /node_modules/
+            },
+            {
                 test: /\.ts$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
-            }
+            },
         ]
     },
     resolve: {
         extensions: ['.ts', '.js']
     },
     plugins: [
-        new CleanPlugin.CleanWebpackPlugin()
+        new MiniCssExtractPlugin({
+            filename: "bundle.[fullhash].css"
+        }),
+        new CleanWebpackPlugin()
     ]
 };
