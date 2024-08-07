6c24ae9dd3fd18a8af61b2d24810f8564233997e
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-08-07 22:59:45 +0200

    Update ToDoList.ts

--- JustDoIt/src/models/ToDoList.ts
+++ JustDoIt/src/models/ToDoList.ts
@@ -2,6 +2,7 @@ import IStorage from "../interfaces/IStorage";
 import ToDoItem from "./ToDoItem";
 import { ToDoItemTyped } from "../types/ToDoItemTyped";
 import { ToDoType } from "../enums/ToDoType";
+import { ToJSON, FromJSON } from "../utils/ToDoItemConverter";
 
 export default class ToDoList implements IStorage<ToDoItem> {
     private static _instance: ToDoList | null = null;
@@ -18,30 +19,17 @@ export default class ToDoList implements IStorage<ToDoItem> {
     }
 
     public Save(): void {
-        localStorage.setItem("todolist", JSON.stringify(this.list.map((toDoItem: ToDoItem) => this.ToJSON(toDoItem))));
+        localStorage.setItem("todolist", JSON.stringify(this.list.map((toDoItem: ToDoItem) => ToJSON(toDoItem))));
     }
 
     public Load(): void {
         this.list = (JSON.parse(localStorage.getItem("todolist") ?? "[]") as ToDoItemTyped[])
-            .map((toDoItemTyped: ToDoItemTyped) => this.FromJSON(toDoItemTyped));
+            .map((toDoItemTyped: ToDoItemTyped) => FromJSON(toDoItemTyped));
     }
 
     public Clear(): void {
         this.list = [];
         this.Save();
-    }
-
-    private ToJSON(toDoItem: ToDoItem): ToDoItemTyped {
-        return {
-            _id: toDoItem.Id,
-            _title: toDoItem.Title,
-            _description: toDoItem.Description,
-            _toDoType: toDoItem.ToDoType
-        };
-    }
-
-    private FromJSON(toDoItemTyped: ToDoItemTyped): ToDoItem {
-        return new ToDoItem(toDoItemTyped._title, toDoItemTyped._description, toDoItemTyped._toDoType, toDoItemTyped._id);
     }
 
     public Add(obj: ToDoItem): void {
