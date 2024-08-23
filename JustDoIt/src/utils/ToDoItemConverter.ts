5e862b5f7ec637ded58a78111f13d147d0ef277e
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-08-23 21:34:55 +0200

    Update ToDoItemConverter.ts

--- JustDoIt/src/utils/ToDoItemConverter.ts
+++ JustDoIt/src/utils/ToDoItemConverter.ts
@@ -6,10 +6,11 @@ export function ToJSON(toDoItem: ToDoItem): ToDoItemTyped {
         _id: toDoItem.Id,
         _title: toDoItem.Title,
         _description: toDoItem.Description,
-        _toDoType: toDoItem.ToDoType
+        _toDoType: toDoItem.ToDoType,
+        _date: toDoItem._date
     };
 }
 
 export function FromJSON(toDoItemTyped: ToDoItemTyped): ToDoItem {
-    return new ToDoItem(toDoItemTyped._title, toDoItemTyped._description, toDoItemTyped._toDoType, toDoItemTyped._id);
+    return new ToDoItem(toDoItemTyped._title, toDoItemTyped._description, toDoItemTyped._toDoType, toDoItemTyped._date, toDoItemTyped._id);
 }
