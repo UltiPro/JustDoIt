60029edffbdda33e8bba9da4baf8d1de59f65379
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-08-24 12:37:02 +0200

    Update edit

--- JustDoIt/src/utils/FormModal.ts
+++ JustDoIt/src/utils/FormModal.ts
@@ -1,11 +1,12 @@
 import ToDoList from "../models/ToDoList";
 import ToDoItem from "../models/ToDoItem";
-import { ToDoType } from "../enums/ToDoType";
 
 const toDoModal = document.getElementById("modal")! as HTMLDivElement;
 const toDoForm = document.getElementById("todo-form")! as HTMLFormElement;
 
 const toDoForm_h2: HTMLHeadingElement = toDoModal.querySelector("h2")! as HTMLHeadingElement;
+const toDoForm_title: HTMLInputElement = toDoForm.querySelector("#todo-form-title")! as HTMLInputElement;
+const toDoForm_description: HTMLInputElement = toDoForm.querySelector("#todo-form-description")! as HTMLInputElement;
 const toDoForm_button: HTMLButtonElement = toDoForm.querySelector("button")! as HTMLButtonElement;
 
 export function InitFormModal(): void {
@@ -26,15 +27,11 @@ export function InitFormModal(): void {
 
 function Add(e: SubmitEvent): void {
     e.preventDefault();
-    const formData: FormData = new FormData(toDoForm);
-    ToDoList.Instance.Add(new ToDoItem(
-        formData.get("todo-form-title")! as string,
-        formData.get("todo-form-description")! as string,
-        +formData.get("todo-form-icon")! as ToDoType));
-    toDoModal.style.display = 'none';
+    ToDoList.Instance.Add(new ToDoItem(toDoForm_title.value, toDoForm_description.value, +(toDoForm.querySelector("input[name='todo-form-icon']:checked")! as HTMLInputElement).value));
+    toDoModal.style.display = "none";
 }
 
-export async function EditForm(): Promise<FormData | null> {
+export async function EditForm(toDoItem: ToDoItem): Promise<FormData | null> {
     toDoForm_h2.textContent = "Edit Task";
     toDoForm_button.textContent = "Edit";
     toDoForm.removeEventListener("submit", Add);
@@ -48,10 +45,13 @@ export async function EditForm(): Promise<FormData | null> {
                 reject(null);
             }
             finally {
-                toDoModal.style.display = 'none';
+                toDoModal.style.display = "none";
             }
         }, { once: true });
         toDoForm.reset();
+        toDoForm_title.value = toDoItem.Title;
+        toDoForm_description.value = toDoItem.Description;
+        (toDoForm.querySelector(`#todo-form-icon-${toDoItem.ToDoType}`) as HTMLInputElement).checked = true;
         toDoModal.style.display = "block";
     });
 }
