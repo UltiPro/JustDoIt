59020c0aa7175b7469f69d62a933072e1e6edec1
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-08-29 12:56:09 +0200

    Webpack reconfig 3/3

--- JustDoIt/webpack.config.prod.js
+++ JustDoIt/webpack.config.prod.js
@@ -1,60 +1,64 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
-    entry: './src/app.ts',
+    mode: "production",
+    entry: "./src/app.ts",
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: "main.[contenthash].js",
+        path: path.resolve(__dirname, "dist")
     },
     optimization: {
         minimizer: [
             `...`,
-            new CssMinimizerPlugin()
-        ],
+            new CssMinimizerPlugin(),
+            new HtmlWebpackPlugin({
+                template: "./index.html",
+                filename: "index.html",
+                minify: {
+                    removeAttributeQuotes: true,
+                    collapseWhitespace: true,
+                    removeComments: true
+                }
+            })
+        ]
     },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: "style.[contenthash].css" }),
+        new CleanWebpackPlugin()
+    ],
     module: {
         rules: [
             {
+                test: /\.html$/,
+                use: "html-loader"
+            },
+            {
+                test: /\.(svg|png|jpg|gif|ico)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "img/[name][ext]"
+                }
+            },
+            {
                 test: /\.css$/,
                 use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: path.resolve(__dirname, 'img')
-                        },
-                    },
+                    MiniCssExtractPlugin.loader,
                     "css-loader"
                 ],
                 exclude: /node_modules/
             },
             {
                 test: /\.ts$/,
-                use: 'ts-loader',
+                use: "ts-loader",
                 exclude: /node_modules/
-            },
+            }
         ]
     },
     resolve: {
-        extensions: ['.ts', '.js']
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./index.html",
-            filename: "index.html",
-            minify: {
-                removeAttributeQuotes: true,
-                collapseWhitespace: true,
-                removeComments: true
-            }
-        }),
-        new MiniCssExtractPlugin({
-            filename: "style.css"
-        }),
-        new CleanWebpackPlugin()
-    ]
+        extensions: [".ts", ".js"]
+    }
 };
